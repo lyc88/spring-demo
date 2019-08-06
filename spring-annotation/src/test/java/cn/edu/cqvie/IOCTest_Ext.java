8415661dbf62716c60176485e2df1a7502e88ff0
382333604@qq.com <xxx@xxx.com>
2019-08-06 23:21:40 +0800

    修改：Spring 生命周期解释

--- spring-annotation/src/test/java/cn/edu/cqvie/IOCTest_Ext.java
+++ spring-annotation/src/test/java/cn/edu/cqvie/IOCTest_Ext.java
@@ -1,15 +1,9 @@
 package cn.edu.cqvie;
 
-import cn.edu.cqvie.bean.Blue;
-import cn.edu.cqvie.bean.Person;
-import cn.edu.cqvie.config.MainConfig;
-import cn.edu.cqvie.config.MainConfig2;
 import cn.edu.cqvie.ext.ExtConfig;
 import org.junit.Test;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.core.env.Environment;
 
 import java.util.Map;
 import java.util.stream.Stream;
